ac9327b06c68d94b06b07277e113dcc1c84e3f6d
mostafa <mostafafatayri@yahoo.com>
2025-03-06 22:56:29 +0200

    test 4

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Ensure correct base path
+  server: {
+    fs: {
+      strict: false
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
